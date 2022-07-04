4f550c92e69cf20654d39ef0f7af2251941e1ac4
iDestyKK <clara@claranguyen.me>
2022-07-04 18:41:24 -0400

    Added argument parsing and minor AR code validation

--- c/src/ards_game_ls.c
+++ c/src/ards_game_ls.c
@@ -22,10 +22,91 @@
 #include "../lib/ards_util/io.h"
 
 // ----------------------------------------------------------------------------
+// Basic Argument Parser                                                   {{{1
+// ----------------------------------------------------------------------------
+
+typedef struct ARGS_T {
+	uint8_t flag_error,
+	        flag_skip_name;
+} args_t;
+
+void print_help(int argc, char **argv) {
+	printf("usage: %s [-ehn] IN_ARDS.nds\n", argv[0]);
+	printf("Listing utility for game addresses in an Action Replay DS ROM "
+		"dump.\n\n");
+
+	printf("Optional arguments are:\n\n");
+
+	printf("\t-e\tPrints errors. By default, this will only print out games "
+		"where a code\n\t\tsection check looks correct. With this flag, it"
+		" will additionally print\n\t\terrors to stderr.\n\n");
+
+	printf("\t-h\tPrints this help prompt in the terminal and then "
+		"terminates.\n\n");
+
+	printf("\t-n\tSkips name reading. By default, this will read a game "
+		"header, validate\n\t\tthe code section, and then run 2n C-Style "
+		"string reads. This argument\n\t\ttells it to skip the final step "
+		"and try to read the next game through\n\t\tthose string sections.\n");
+
+	exit(0);
+}
+
+void parse_flags(int argc, char **argv, args_t *obj) {
+	int i, j, len;
+
+	// Defaults
+	obj->flag_error     = 0;
+	obj->flag_skip_name = 0;
+
+	// Go through every argument and read characters
+	for (i = 1; i < argc; i++) {
+		// Skip if not a valid flag header
+		if (argv[i][0] != '-')
+			continue;
+
+		len = strlen(argv[i]);
+		for (j = 1; j < len; j++) {
+			switch (argv[i][j]) {
+				case 'e':
+					// Show errors
+					obj->flag_error = 1;
+					break;
+
+				case 'h':
+					// Shows help prompt and terminate
+					print_help(argc, argv);
+					break;
+
+				case 'n':
+					// Skips name reading
+					obj->flag_skip_name = 1;
+					break;
+				default:
+					// Invalid Flag
+					fprintf(
+						stderr,
+						"WARN: Invalid flag \"%c\" was given. Ignoring...\n",
+						argv[i][j]
+					);
+
+					break;
+			}
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
 // Verification Functions                                                  {{{1
 // ----------------------------------------------------------------------------
 
-int verify_code_segment(unsigned char *buf, size_t len, uint16_t num_codes) {
+int verify_code_segment(
+	unsigned char *buf,
+	size_t         len,
+	uint16_t       num_codes,
+	args_t        *args,
+	size_t        *err_pos
+) {
 	size_t    pos, i, j, c_found;
 	ar_flag_t flag, in_flag;
 	uint16_t  num , in_num ;
@@ -59,30 +140,38 @@ int verify_code_segment(unsigned char *buf, size_t len, uint16_t num_codes) {
 					in_flag = (ar_flag_t) (*(uint8_t  *) &buf[pos    ]);
 					in_num  =              *(uint16_t *) &buf[pos + 2] ;
 
-					if (in_flag != AR_FLAG_CODE)
+					if (in_flag != AR_FLAG_CODE) {
 						// Flag inside folder was not a code
+						*err_pos = pos;
 						return 2;
+					}
 
 					pos += 4 + (8 * in_num);
 					c_found++;
 
-					if (c_found == num_codes && j < num - 1)
+					if (c_found == num_codes && j < num - 1) {
 						// More codes than mentioned in header
+						*err_pos = pos;
 						return 4;
+					}
 				}
 
 				break;
 
 			case AR_FLAG_TERMINATE:
 				if (c_found == num_codes)
+					// Looks good to me
 					return 0;
-				else
+				else {
 					// More codes than mentioned in header
+					*err_pos = pos;
 					return 4;
+				}
 				break;
 
 			default:
 				// Invalid flag was found
+				*err_pos = pos;
 				return 1;
 		}
 	}
@@ -97,15 +186,20 @@ int verify_code_segment(unsigned char *buf, size_t len, uint16_t num_codes) {
 
 int main(int argc, char **argv) {
 	// Argument check
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s IN_ARDS.nds\n", argv[0]);
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s [-ehn] IN_ARDS.nds\n", argv[0]);
 
 		return 1;
 	}
 
+	// Parse arguments
+	args_t args;
+
+	parse_flags(argc, argv, &args);
+
 	FILE          *fp;
 	ar_game_info_t header;
-	size_t         pos, i, fsize;
+	size_t         pos, i, fsize, err_at;
 	char          *name;
 	char          *shit;
 	unsigned char *buf;
@@ -116,7 +210,12 @@ int main(int argc, char **argv) {
 	shit = NULL;
 
 	// Setup file for traversal
-	fp = fopen(argv[1], "rb");
+	// The first argument without a "-" is the filename.
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] != '-')
+			break;
+	}
+	fp = fopen(argv[i], "rb");
 
 	// Get size of file
 	fseek(fp, 0, SEEK_END);
@@ -157,13 +256,68 @@ int main(int argc, char **argv) {
 		status = verify_code_segment(
 			buf,
 			header.code_bytes_size - 32,
-			header.num_codes
+			header.num_codes,
+			&args,
+			&err_at
 		);
 
 		free(buf);
 
 		if (status != 0) {
 			fseek(fp, pos + 1, SEEK_SET);
+			if (args.flag_error == 1) {
+				switch (status) {
+					case 1:
+						fprintf(
+							stderr,
+							"Error 0x%08x + 0x%08x: %s\n",
+							pos,
+							err_at + 32,
+							"Invalid flag was found"
+						);
+						break;
+
+					case 2:
+						fprintf(
+							stderr,
+							"Error 0x%08x + 0x%08x: %s\n",
+							pos,
+							err_at + 32,
+							"Flag inside folder was not a code"
+						);
+						break;
+
+					case 3:
+						fprintf(
+							stderr,
+							"Error 0x%08x + 0x%08x: %s\n",
+							pos,
+							err_at + 32,
+							"Exceeded buffer size"
+						);
+						break;
+
+					case 4:
+						fprintf(
+							stderr,
+							"Error 0x%08x + 0x%08x: %s\n",
+							pos,
+							err_at + 32,
+							"More codes than mentioned in header"
+						);
+						break;
+
+					default:
+						fprintf(
+							stderr,
+							"Error 0x%08x + 0x%08x: %s\n",
+							pos,
+							err_at + 32,
+							"Undocumented error"
+						);
+						break;
+				}
+			}
 			continue;
 		}
 
@@ -177,9 +331,11 @@ int main(int argc, char **argv) {
 		printf("0x%08x - %s\n", pos, name);
 
 		// Skip all codes afterwards
-		for (i = 0; i < header.num_codes; i++) {
-			if (shit != NULL) free(shit); shit = file_read_string(fp);
-			if (shit != NULL) free(shit); shit = file_read_string(fp);
+		if (args.flag_skip_name != 1) {
+			for (i = 0; i < header.num_codes; i++) {
+				if (shit != NULL) free(shit); shit = file_read_string(fp);
+				if (shit != NULL) free(shit); shit = file_read_string(fp);
+			}
 		}
 	}
 
